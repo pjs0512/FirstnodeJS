5b7cc204c7f1a60142ce5bcf4ca3297167e79e6a
pjs0512 <egt5001@naver.com>
2017-11-15 21:57:57 +0900

    mongo 이게몽고?

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ var path = require('path');
 
 var app = express();
 
+app.set("view engine",'ejs');
 app.use(express.static(path.join(__dirname, 'public'))); // public 을 사용하겠다.
 
+app.get('/',function (req,res) {
+    res.render('my_first_ejs');
+});
 app.listen(3000,function () { // 서버 실행
 
     console.log('Server On!'); // 서버 실행시 보내는 메세지
